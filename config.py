d040fa44355fe1a7f3cff68b031a40837a8f26cc
Eveeee <44003480+katsu-chan@users.noreply.github.com>
2024-03-17 23:15:55 +0300

    fixed things that i did

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
-
+import os
 
 mod = "mod4"
 # terminal = guess_terminal()
